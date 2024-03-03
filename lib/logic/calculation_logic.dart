c7c0f5126e4fb49d78c467a725682dcd44e879db
Mike-205 <mikemwongela1025@gmail.com>
2024-03-03 14:12:07 +0300

    business logic

--- lib/logic/calculation_logic.dart
+++ lib/logic/calculation_logic.dart
@@ -13,4 +13,38 @@ class CalculateLogic {
         return 0;
     }
   }
+
+  num evaluateExpression(String expression) {
+    List<String> numbers = expression.split(RegExp(r'[+\-×/]'));
+    List<String> operators = expression.split(RegExp(r'[^+\-×/]')).where((s) => s.isNotEmpty).toList();
+
+    for (int i = 0; i < operators.length; i++) {
+      if (operators[i] == "×" || operators[i] == "/") {
+        num result = operators[i] == "×" ? num.parse(numbers[i]) * num.parse(numbers[i + 1]) : num.parse(numbers[i]) / num.parse(numbers[i + 1]);
+        numbers[i] = result.toString();
+        numbers.removeAt(i + 1);
+        operators.removeAt(i);
+        i--;
+      }
+    }
+
+    for (int i = 0; i < operators.length; i++) {
+      num result = operators[i] == "+" ? num.parse(numbers[i]) + num.parse(numbers[i + 1]) : num.parse(numbers[i]) - num.parse(numbers[i + 1]);
+      numbers[i] = result.toString();
+      numbers.removeAt(i + 1);
+      operators.removeAt(i);
+      i--;
+    }
+
+    num finalResult = num.parse(numbers[0]);
+    String formattedResult = finalResult.toString();
+
+    // Check if the final result is a decimal number
+    if (finalResult % 1 != 0) {
+      formattedResult = finalResult.toStringAsFixed(2); // Format the result with 3 decimal places
+      formattedResult = formattedResult.replaceAll(RegExp(r'\.?0+$'), '');// Remove trailing zeros
+    }
+
+    return num.parse(formattedResult);
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,7 +34,7 @@ class _HomePageState extends State<HomePage> {
         display += text; // Append operation to display
       }
       else if (text == "=") {
-        checker = evaluateExpression(display);
+        checker = calculateLogic.evaluateExpression(display);
         if (checker is double){
           result = checker.toStringAsFixed(2);
         }
@@ -51,40 +51,6 @@ class _HomePageState extends State<HomePage> {
         }
       }
     });
-  }
-
-  num evaluateExpression(String expression) {
-    List<String> numbers = expression.split(RegExp(r'[+\-×/]'));
-    List<String> operators = expression.split(RegExp(r'[^+\-×/]')).where((s) => s.isNotEmpty).toList();
-
-    for (int i = 0; i < operators.length; i++) {
-      if (operators[i] == "×" || operators[i] == "/") {
-        num result = operators[i] == "×" ? num.parse(numbers[i]) * num.parse(numbers[i + 1]) : num.parse(numbers[i]) / num.parse(numbers[i + 1]);
-        numbers[i] = result.toString();
-        numbers.removeAt(i + 1);
-        operators.removeAt(i);
-        i--;
-      }
-    }
-
-    for (int i = 0; i < operators.length; i++) {
-      num result = operators[i] == "+" ? num.parse(numbers[i]) + num.parse(numbers[i + 1]) : num.parse(numbers[i]) - num.parse(numbers[i + 1]);
-      numbers[i] = result.toString();
-      numbers.removeAt(i + 1);
-      operators.removeAt(i);
-      i--;
-    }
-
-    num finalResult = num.parse(numbers[0]);
-    String formattedResult = finalResult.toString();
-
-    // Check if the final result is a decimal number
-    if (finalResult % 1 != 0) {
-      formattedResult = finalResult.toStringAsFixed(2); // Format the result with 3 decimal places
-      formattedResult = formattedResult.replaceAll(RegExp(r'\.?0+$'), '');// Remove trailing zeros
-    }
-
-    return num.parse(formattedResult);
   }
 
   @override
